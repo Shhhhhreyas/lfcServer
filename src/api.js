ff00b2cc7441b48eaadbf8a90787fa936f49295a
Shhhhhreyas <shreyashondadakatti@airasia.com>
2025-03-17 16:03:26 +0530

    [FEAT] Add microSite Data

--- src/api.js
+++ src/api.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const serverless = require("serverless-http");
 
+const bodyParser = require("body-parser");
+
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 const cors = require("cors");
 app.use(cors());
+/* const admin = require("firebase-admin");
+const serviceAccount = require("./customer-journey-stg-firebase-adminsdk-okzlk-8f31eb70c3.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+}); */
+//app.use(bodyParser.text());
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(upload.array());
+
 const router = express.Router();
 const fns = require("date-fns");
 const date_format = "dd/MM/yyyy";
@@ -233,6 +247,155 @@ router.post("/fp/xsell/aggregator/v2/page-results", (req, res) => {
   } else {
     res.json(require("./v2OneWay.json"));
   }
+});
+
+router.post("/didDriverAccept", (req, res) => {
+  const { nearbyDrivers, pollCount } = req.body;
+  console.log("See: ", pollCount);
+  if (pollCount === 3) {
+    res.json({
+      driverInfo:
+        nearbyDrivers[randomIntFromInterval(0, nearbyDrivers.length - 1)],
+    });
+  } else {
+    res.json(null);
+  }
+});
+
+router.post("/didDriverStartDelivery", (req, res) => {
+  const { deliveryLocation, pollCount } = req.body;
+  if (pollCount >= 4) {
+    res.json({ deliveryLocation, deliveryStarted: true });
+  } else {
+    res.json({ deliveryStarted: false });
+  }
+});
+
+router.post("/getDriverLocation", (req, res) => {
+  const { polyline, pollCount } = req.body;
+  if (pollCount >= 1 && polyline.length > 1) {
+    console.log("pollCount: ", pollCount);
+    console.log("Sending:  ", polyline.slice(1));
+    res.json({ polyline: polyline.slice(1) });
+  } else {
+    res.json({ polyline: polyline });
+  }
+});
+router.post("/getTrips", (req, res) => {
+  const trip = {
+    shopInfo: {
+      business_status: "OPERATIONAL",
+      geometry: {
+        location: {
+          lat: 22.7216897,
+          lng: 75.8786025,
+        },
+        viewport: {
+          northeast: {
+            lat: 22.72307622989272,
+            lng: 75.88000757989272,
+          },
+          southwest: {
+            lat: 22.72037657010728,
+            lng: 75.87730792010727,
+          },
+        },
+      },
+      icon: "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/shopping-71.png",
+      icon_background_color: "#4B96F3",
+      icon_mask_base_uri:
+        "https://maps.gstatic.com/mapfiles/place_api/icons/v2/shopping_pinlet",
+      name: "Treasure Island Mall (TI Mall)",
+      opening_hours: {
+        open_now: true,
+      },
+      photos: [
+        {
+          height: 4096,
+          html_attributions: [
+            '<a href="https://maps.google.com/maps/contrib/114630510192412077490">Yogesh Meena</a>',
+          ],
+          photo_reference:
+            "AWU5eFizA3Mz5kwYXSYy8G-8s4dcAgzc0jvcTrRcm6cNM7qJc5uTIPDBCsMfCKhsTrFHXlmEi5rVAF--Rq6FV74lLzUiN7UioDfDJXIP75k84MLHWEyGYjuwt1BOVBqNsFd6BxwOLzh6FIoNNtAS8P5bPvaA4Gea1fc9o6_o5963718XwdRW",
+          width: 3072,
+        },
+      ],
+      place_id: "ChIJAQAAQBb9YjkR8-h83fKABeI",
+      plus_code: {
+        compound_code: "PVCH+MC Indore, Madhya Pradesh, India",
+        global_code: "7JJQPVCH+MC",
+      },
+      rating: 4.3,
+      reference: "ChIJAQAAQBb9YjkR8-h83fKABeI",
+      scope: "GOOGLE",
+      types: ["shopping_mall", "point_of_interest", "establishment"],
+      user_ratings_total: 34463,
+      vicinity: "11, Mahatma Gandhi Rd, South Tukoganj, Indore",
+    },
+    deliveryLocation: {
+      _id: "658315fa896f430017f2c8dc",
+      addressTitle: "Home",
+      house: "345",
+      address: {
+        name: "303,  Clerk Colony",
+        value: "Indore, Madhya Pradesh 452010, India",
+      },
+      coords: {
+        latitude: 22.7487484,
+        longitude: 75.8715503,
+      },
+      floor: 0,
+      apartment: "Nilay",
+      instructions: "",
+      customerName: "Test7 Test7",
+      customerContactNumber: "+919999999997",
+    },
+  };
+  const numberOfTrips = randomIntFromInterval(0, 4);
+
+  if (numberOfTrips) {
+    res.json({
+      trips: Array(numberOfTrips)
+        .fill(trip)
+        .map((newTrip) => {
+          const totalDistance = Math.round(Math.random() * 10000) / 100;
+          const distanceToPickupLocation =
+            Math.round(Math.random() * 1000) / 100;
+          const distanceToDeliveryLocation =
+            totalDistance - distanceToPickupLocation;
+          return {
+            ...newTrip,
+            tripId: Math.random().toString(36).split(".")[1],
+            type: Math.random() > 0.5 ? "Grocery Delivery" : "Parcel delivery",
+            distanceToDeliveryLocation: distanceToDeliveryLocation + "km",
+            estimatedTimeOfTrip:
+              Math.round(Math.random() * 10000) / 100 + " minutes",
+            distanceToPickupLocation: distanceToPickupLocation + "km",
+            totalDistance: totalDistance + "km",
+          };
+        }),
+    });
+  } else {
+    res.json({ trips: [{ noTrips: true }] });
+  }
+});
+
+router.post("/getCustomToken", async (req, res) => {
+  const { userId } = req.body;
+  try {
+    const customToken = await admin.auth().createCustomToken(userId);
+    res.status(200).json({ token: customToken });
+  } catch (error) {
+    console.error("Error creating custom token:", error);
+    res.status(500);
+    res.json({ error });
+  }
+});
+
+router.post("/micrositeData", (req, res) => {
+  res.set({ "Content-Type": "application/json" });
+
+  res.json(require("./proposed-content.json"));
 });
 
 app.use(`/.netlify/functions/api`, router);
